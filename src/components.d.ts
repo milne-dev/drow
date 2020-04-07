cb55e85ced6e21e4ee814cdf063d2f79543d7a6e
Derek Milne <derek.m.m.77@gmail.com>
2020-04-06 20:16:56 -0700

    remove unused files

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,68 +6,32 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-  interface DRow {}
-  interface MyComponent {
-    /**
-     * The first name
-     */
-    first: string;
-    /**
-     * The last name
-     */
-    last: string;
-    /**
-     * The middle name
-     */
-    middle: string;
-  }
+    interface DRow {
+    }
 }
 declare global {
-  interface HTMLDRowElement extends Components.DRow, HTMLStencilElement {}
-  var HTMLDRowElement: {
-    prototype: HTMLDRowElement;
-    new (): HTMLDRowElement;
-  };
-  interface HTMLMyComponentElement
-    extends Components.MyComponent,
-      HTMLStencilElement {}
-  var HTMLMyComponentElement: {
-    prototype: HTMLMyComponentElement;
-    new (): HTMLMyComponentElement;
-  };
-  interface HTMLElementTagNameMap {
-    "d-row": HTMLDRowElement;
-    "my-component": HTMLMyComponentElement;
-  }
+    interface HTMLDRowElement extends Components.DRow, HTMLStencilElement {
+    }
+    var HTMLDRowElement: {
+        prototype: HTMLDRowElement;
+        new (): HTMLDRowElement;
+    };
+    interface HTMLElementTagNameMap {
+        "d-row": HTMLDRowElement;
+    }
 }
 declare namespace LocalJSX {
-  interface DRow {}
-  interface MyComponent {
-    /**
-     * The first name
-     */
-    first?: string;
-    /**
-     * The last name
-     */
-    last?: string;
-    /**
-     * The middle name
-     */
-    middle?: string;
-  }
-  interface IntrinsicElements {
-    "d-row": DRow;
-    "my-component": MyComponent;
-  }
+    interface DRow {
+    }
+    interface IntrinsicElements {
+        "d-row": DRow;
+    }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
-  export namespace JSX {
-    interface IntrinsicElements {
-      "d-row": LocalJSX.DRow & JSXBase.HTMLAttributes<HTMLDRowElement>;
-      "my-component": LocalJSX.MyComponent &
-        JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+    export namespace JSX {
+        interface IntrinsicElements {
+            "d-row": LocalJSX.DRow & JSXBase.HTMLAttributes<HTMLDRowElement>;
+        }
     }
-  }
 }
